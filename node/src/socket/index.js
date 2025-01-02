ad74dc7a994d461d19edcd23e6684312329c6be1
ahmedessam-7 <ae.0x70@gmail.com>
2025-01-02 15:44:53 +0200

    feat: Broadcast web socket messages on socket message received

--- node/src/socket/index.js
+++ node/src/socket/index.js
@@ -1,8 +1,19 @@
 import net from 'net';
+import lodash from 'lodash';
+
+import ws from '../ws';
 
 import config from '../config';
 
 const socket = new net.Socket();
+
+socket.on('data', (buffer) => {
+  const message = buffer.toString('utf8');
+
+  lodash.forEach([...ws.clients], (client) => {
+    client.send(message);
+  });
+});
 
 socket.establish = () => socket.connect({ host: config.socket.host, port: config.socket.port });
 
